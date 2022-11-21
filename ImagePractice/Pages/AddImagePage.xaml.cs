194c1b573caf6d45dbfad182d3ddb157fce3e60b
Радмир Габдрахманов <gabdrahmanov.radmir2018@gmail.com>
2022-11-21 15:07:44 +0300

    Add project files.

---
+++ ImagePractice/Pages/AddImagePage.xaml.cs
@@ -0,0 +1,70 @@
+﻿using ImagePractice.ADOApp;
+using Microsoft.Win32;
+using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace ImagePractice.Pages
+{
+    /// <summary>
+    /// Interaction logic for AddImagePage.xaml
+    /// </summary>
+    public partial class AddImagePage : Page
+    {
+        public byte[] ImageMat { get; set; }
+
+        public AddImagePage()
+        {
+            InitializeComponent();
+        }
+
+        private void ImageSelectionButtonClick(object sender, RoutedEventArgs e)
+        {
+            try
+            {
+                var btnSelect = sender as Button;
+                OpenFileDialog dialog = new OpenFileDialog();
+                if(dialog.ShowDialog() != null)
+                {
+                    ImageMat = File.ReadAllBytes(dialog.FileName);
+                    btnSelect.Background = Brushes.Green;
+                }
+            }
+            catch
+            {
+                MessageBox.Show("Только фото!", "Ошибка");
+            }
+
+        }
+
+        private void SaveMaterialClick(object sender, RoutedEventArgs e)
+        {
+            Material material = new Material
+            {
+                Title = tbTitle.Text,
+                Info = tbInfo.Text,
+                ByteImage = ImageMat
+            };
+
+            App.Connection.Material.Add(material);
+            App.Connection.SaveChanges();
+        }
+
+        private void GoToImageGalleryPage(object sender, RoutedEventArgs e)
+        {
+            NavigationService.Navigate(new ImageGalleryPage());
+        }
+    }
+}
---
+++ ImagePractice/Pages/ImageGalleryPage.xaml.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace ImagePractice.Pages
+{
+    /// <summary>
+    /// Interaction logic for ImageGalleryPage.xaml
+    /// </summary>
+    public partial class ImageGalleryPage : Page
+    {
+        public ImageGalleryPage()
+        {
+            InitializeComponent();
+            listImages.ItemsSource = App.Connection.Material.ToList();
+        }
+
+        private void GoToImageAddPage(object sender, RoutedEventArgs e)
+        {
+            NavigationService.Navigate(new ImageGalleryPage());
+        }
+    }
+}
